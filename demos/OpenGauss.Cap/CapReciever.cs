ad94f42c9d3b29d1a26faeec279be8d566a482b6
林艺伟 <linyiwei@szyjian.com>
2024-10-29 11:54:03 +0800

    feature: DotnetCore.CAP.OpenGauss dotnet 6.0

---
+++ demos/OpenGauss.Cap/CapReciever.cs
@@ -0,0 +1,24 @@
+using DotNetCore.CAP;
+using Microsoft.AspNetCore.Mvc;
+using OpenGauss.Cap.Controllers;
+
+namespace OpenGauss.Cap
+{
+    public class CapReciever : ICapSubscribe
+    {
+        private readonly ILogger<CapPublishController> _logger;
+        private readonly ICapPublisher _capPublisher;
+
+        public CapReciever(ILogger<CapPublishController> logger, ICapPublisher capPublisher)
+        {
+            _logger = logger;
+            _capPublisher = capPublisher;
+        }
+
+        [CapSubscribe("cap.publish.test")]
+        public async Task Recieved(object message)
+        {
+            _logger.LogInformation("收到消息：{@Message}", message);
+        }
+    }
+}
---
+++ src/DotNetCore.CAP.OpenGauss/CAP.OpenGaussCapOptionsExtension.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) .NET Core Community. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using DotNetCore.CAP.Persistence;
+using DotNetCore.CAP.OpenGauss;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+// ReSharper disable once CheckNamespace
+namespace DotNetCore.CAP
+{
+    internal class OpenGaussCapOptionsExtension : ICapOptionsExtension
+    {
+        private readonly Action<OpenGaussOptions> _configure;
+
+        public OpenGaussCapOptionsExtension(Action<OpenGaussOptions> configure)
+        {
+            _configure = configure;
+        }
+
+        public void AddServices(IServiceCollection services)
+        {
+            services.AddSingleton<CapStorageMarkerService>();
+            services.Configure(_configure);
+            services.AddSingleton<IConfigureOptions<OpenGaussOptions>, ConfigureOpenGaussOptions>();
+
+            services.AddSingleton<IDataStorage, OpenGaussDataStorage>();
+            services.AddSingleton<IStorageInitializer, OpenGaussStorageInitializer>();
+        }
+    }
+}
